85f099d014b3163ddf672237964c642ad72e52f6
Allison Serrano <allisonserrano171@yahoo.com>
2023-09-30 18:02:42 -0400

    doing it another way

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://allicristine:Alicris051@cluster0.ovhrboy.mongodb.net/?retryWrites=true&w=majority', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', 
 // {
 // useNewUrlParser: true,
 // useUnifiedTopology: true,
